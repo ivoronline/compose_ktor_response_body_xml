ed49d11664b2288d4bc72e15201cb1c3481044b8
igoreta <ivoronline@gmail.com>
2022-05-24 10:32:32 +0200

    Initial commit

---
+++ app/src/main/java/com/ivoronline/compose_ktor_response_body_xml/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.ivoronline.compose_ktor_response_body_xml
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.xml.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+
+//==================================================================
+// MAIN ACTIVITY
+//==================================================================
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+
+      var person by remember { mutableStateOf(PersonDTOXML(0, "No name", 0)) }
+      val coroutineScope = rememberCoroutineScope()
+
+      Button(onClick = { coroutineScope.launch { person = callURL() } }) {
+        Text("RESPONSE: $person")
+      }
+
+    }
+  }
+}
+
+//==================================================================
+// CALL URL
+//==================================================================
+suspend fun callURL() : PersonDTOXML {
+
+  //CONFIGURE CLIENT
+  val client = HttpClient(CIO) {
+    install(ContentNegotiation){ xml() }
+  }
+
+  //CAL URL
+  val person: PersonDTOXML = client.get("http://192.168.0.102:8080/ReceiveBodyXML").body()
+
+  //CLOSE CLIENT
+  client.close()
+
+  //RETURN PERSON
+  println(person)  //Person(id=1, name=John, age=20)
+  return person;
+
+}
+
+//==================================================================
+// PERSONDTOXML
+//==================================================================
+@Serializable
+data class PersonDTOXML(val id: Int, val name: String, val age: Int)
